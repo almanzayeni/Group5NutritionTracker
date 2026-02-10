1d97c481a0e96cc86ef48e07256640b5c55f4b5e
jsmith5721 <161377944+jsmith5721@users.noreply.github.com>
2026-02-10 11:22:04 -0500

    Created Maven Base Project
    I added the base maven project.

---
+++ code/NutritionTracker/src/test/java/CS3212Group5/NutritionTracker/AppTest.java
@@ -0,0 +1,13 @@
+package CS3212Group5.NutritionTracker;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    @Test
+	public void testApp() {
+    	assert(true);
+    }
+}
