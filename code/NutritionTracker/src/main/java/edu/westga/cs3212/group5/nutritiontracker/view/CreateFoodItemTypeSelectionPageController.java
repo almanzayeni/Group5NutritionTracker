421fc3a46afa07c1bb1f60e29bd9c22974a6d59c
Justin Smith <jsmith90@my.westga.edu>
2026-02-18 16:16:45 -0500

    Added Functionality to Navigation Buttons
    Added functionality to the hamburger menu, home button and logout button.
    Also added functionality to the selectBaseFood button and
    selectCompositFood buttons.

--- code/NutritionTracker/src/main/java/edu/westga/cs3212/group5/nutritiontracker/view/CreateFoodItemTypeSelectionPageController.java
+++ code/NutritionTracker/src/main/java/edu/westga/cs3212/group5/nutritiontracker/view/CreateFoodItemTypeSelectionPageController.java
@@ -1,35 +1,37 @@
 package edu.westga.cs3212.group5.nutritiontracker.view;
 
 import com.jfoenix.controls.JFXHamburger;
+import com.jfoenix.transitions.hamburger.HamburgerSlideCloseTransition;
+
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 public class CreateFoodItemTypeSelectionPageController {
-
     @FXML
     private ResourceBundle resources;
-
     @FXML
     private URL location;
-
     @FXML
     private JFXHamburger hamburgerMenu;
-
     @FXML
     private Button homeButton;
-
     @FXML
     private Button logoutButton;
-
     @FXML
     private Pane menuPane;
-
     @FXML
     private Button selectBaseFoodButton;
-
     @FXML
     private Button selectCompositFoodButton;
 
@@ -41,7 +43,132 @@ public class CreateFoodItemTypeSelectionPageController {
         assert menuPane != null : "fx:id=\"menuPane\" was not injected: check your FXML file 'CreateFoodItemTypeSelectionPage.fxml'.";
         assert selectBaseFoodButton != null : "fx:id=\"selectBaseFoodButton\" was not injected: check your FXML file 'CreateFoodItemTypeSelectionPage.fxml'.";
         assert selectCompositFoodButton != null : "fx:id=\"selectCompositFoodButton\" was not injected: check your FXML file 'CreateFoodItemTypeSelectionPage.fxml'.";
-
+        setUpListeners();
     }
+    
+    private void setUpListeners() {
+		this.handleHamburgerMenuClick();
+		//this.setUpListenerForLogoutButton();
+		this.setUpListenerForHomeButton();
+		this.setUpListenerForSelectBaseFoodButton();
+		//this.setUpListenerForSelectCompositeFoodButton();
+	}
+    
+    private void handleHamburgerMenuClick() {
+    	HamburgerSlideCloseTransition transition = new HamburgerSlideCloseTransition(this.hamburgerMenu);
+    	transition.setRate(-1);
+		this.hamburgerMenu.setOnMouseClicked(event -> {
+			try {
+				transition.setRate(transition.getRate() * -1);
+				transition.play();
+				if (menuPane.isVisible()) {
+					menuPane.setVisible(false);
+					homeButton.disableProperty().set(true);
+				} else {
+					menuPane.setVisible(true);
+					homeButton.disableProperty().set(false);
+					menuPane.toFront();
+					hamburgerMenu.toFront();
+					homeButton.toFront();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		});
+    }
+    
+//    private void setUpListenerForLogoutButton() {
+//		this.logoutButton.setOnAction((ActionEvent event) -> {
+//			try {
+//				//this.viewModel.saveData();
+//
+//				FXMLLoader loader = new FXMLLoader();
+//				loader.setLocation(LandingPage.class.getResource("LoginView.fxml"));
+//				loader.load();
+//
+//				Parent parent = loader.getRoot();
+//				Scene scene = new Scene(parent);
+//
+//				Stage stage = (Stage) (((Node) event.getSource()).getScene().getWindow());
+//				stage.setScene(scene);
+//				stage.setTitle("Login");
+//				stage.show();
+//
+//			} catch (Exception ex) {
+//				ex.printStackTrace();
+//				Alert alert = new Alert(Alert.AlertType.ERROR, "Error logging user out.");
+//				alert.showAndWait();
+//			}
+//		});
+//	}
+    
+    private void setUpListenerForHomeButton() {
+    	this.homeButton.setOnAction((ActionEvent event) -> {
+			try {
+				FXMLLoader loader = new FXMLLoader();
+				loader.setLocation(HomeDashboardPageController.class.getResource("HomeDashboardPage.fxml"));
+				loader.load();
+
+				Parent parent = loader.getRoot();
+				Scene scene = new Scene(parent);
+
+				Stage stage = (Stage) (((Node) event.getSource()).getScene().getWindow());
+				stage.setScene(scene);
+				stage.setTitle("Home");
+				stage.show();
+
+			} catch (Exception ex) {
+				ex.printStackTrace();
+				Alert alert = new Alert(Alert.AlertType.ERROR, "Error navigating to home page.");
+				alert.showAndWait();
+			}
+    	});
+    }
+    
+    private void setUpListenerForSelectBaseFoodButton() {
+		this.selectBaseFoodButton.setOnAction((ActionEvent event) -> {
+			try {
+				FXMLLoader loader = new FXMLLoader();
+				loader.setLocation(CreateBaseFoodPageController.class.getResource("CreateBaseFoodPage.fxml"));
+				loader.load();
+
+				Parent parent = loader.getRoot();
+				Scene scene = new Scene(parent);
+
+				Stage stage = (Stage) (((Node) event.getSource()).getScene().getWindow());
+				stage.setScene(scene);
+				stage.setTitle("Create Base Food Item");
+				stage.show();
+
+			} catch (Exception ex) {
+				ex.printStackTrace();
+				Alert alert = new Alert(Alert.AlertType.ERROR, "Error navigating to create base food item page.");
+				alert.showAndWait();
+			}
+		});
+    }
+    
+//    private void setUpListenerForSelectCompositeFoodButton() {
+//    	this.selectCompositFoodButton.setOnAction((ActionEvent event) -> {
+//    		try {
+//    			FXMLLoader loader = new FXMLLoader();
+//				loader.setLocation(CreateCompositFoodPageController.class.getResource("CreateCompositFoodPage.fxml"));
+//				loader.load();
+//
+//				Parent parent = loader.getRoot();
+//				Scene scene = new Scene(parent);
+//
+//				Stage stage = (Stage) (((Node) event.getSource()).getScene().getWindow());
+//				stage.setScene(scene);
+//				stage.setTitle("Create Base Food Item");
+//				stage.show();
+//
+//			} catch (Exception ex) {
+//				ex.printStackTrace();
+//				Alert alert = new Alert(Alert.AlertType.ERROR, "Error navigating to create base food item page.");
+//				alert.showAndWait();
+//			}
+//    	});
+//    }
 
 }
