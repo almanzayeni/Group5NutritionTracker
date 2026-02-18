bd79784e01a35030477fce5db79b34120ecc138a
Justin Smith <jsmith90@my.westga.edu>
2026-02-18 14:52:27 -0500

    Added Navigation Buttons
    I added navigation buttons to the pages for selecting a type of food
    item to create and to the page for creating the BaseFoodItem.
    I also updated the skeletons of the controller classes for both.

--- code/NutritionTracker/src/main/java/edu/westga/cs3212/group5/nutritiontracker/view/CreateBaseFoodPageController.java
+++ code/NutritionTracker/src/main/java/edu/westga/cs3212/group5/nutritiontracker/view/CreateBaseFoodPageController.java
@@ -1,45 +1,78 @@
 package edu.westga.cs3212.group5.nutritiontracker.view;
 
+import com.jfoenix.controls.JFXHamburger;
 import java.net.URL;
 import java.util.ResourceBundle;
-
-import edu.westga.cs3212.group5.nutritiontracker.model.QuantityCategory;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
 
 public class CreateBaseFoodPageController {
+
     @FXML
     private ResourceBundle resources;
+
     @FXML
     private URL location;
+
+    @FXML
+    private Button addFoodButton;
+
     @FXML
     private TextField caloriesTextField;
+
     @FXML
     private TextField carbohydratesTextField;
+
     @FXML
     private TextField fatTextField;
+
+    @FXML
+    private JFXHamburger hamburgerMenu;
+
+    @FXML
+    private Button homeButton;
+
+    @FXML
+    private Button logoutButton;
+
+    @FXML
+    private Pane menuPane;
+
     @FXML
     private TextField nameTextField;
+
     @FXML
     private TextField portionSizeTextField;
+
     @FXML
     private Label portionSizeUnitLabel;
+
     @FXML
     private TextField proteinTextField;
+
     @FXML
-    private ComboBox<QuantityCategory> quantityCategoryComboBox;
+    private ComboBox<?> quantityCategoryComboBox;
+
     @FXML
     private TextField sodiumTextField;
+
     @FXML
     private TextField sugarTextField;
 
     @FXML
     void initialize() {
+        assert addFoodButton != null : "fx:id=\"addFoodButton\" was not injected: check your FXML file 'CreateBaseFoodPage.fxml'.";
         assert caloriesTextField != null : "fx:id=\"caloriesTextField\" was not injected: check your FXML file 'CreateBaseFoodPage.fxml'.";
         assert carbohydratesTextField != null : "fx:id=\"carbohydratesTextField\" was not injected: check your FXML file 'CreateBaseFoodPage.fxml'.";
         assert fatTextField != null : "fx:id=\"fatTextField\" was not injected: check your FXML file 'CreateBaseFoodPage.fxml'.";
+        assert hamburgerMenu != null : "fx:id=\"hamburgerMenu\" was not injected: check your FXML file 'CreateBaseFoodPage.fxml'.";
+        assert homeButton != null : "fx:id=\"homeButton\" was not injected: check your FXML file 'CreateBaseFoodPage.fxml'.";
+        assert logoutButton != null : "fx:id=\"logoutButton\" was not injected: check your FXML file 'CreateBaseFoodPage.fxml'.";
+        assert menuPane != null : "fx:id=\"menuPane\" was not injected: check your FXML file 'CreateBaseFoodPage.fxml'.";
         assert nameTextField != null : "fx:id=\"nameTextField\" was not injected: check your FXML file 'CreateBaseFoodPage.fxml'.";
         assert portionSizeTextField != null : "fx:id=\"portionSizeTextField\" was not injected: check your FXML file 'CreateBaseFoodPage.fxml'.";
         assert portionSizeUnitLabel != null : "fx:id=\"portionSizeUnitLabel\" was not injected: check your FXML file 'CreateBaseFoodPage.fxml'.";
--- code/NutritionTracker/src/main/java/edu/westga/cs3212/group5/nutritiontracker/view/CreateFoodItemTypeSelectionPageController.java
+++ code/NutritionTracker/src/main/java/edu/westga/cs3212/group5/nutritiontracker/view/CreateFoodItemTypeSelectionPageController.java
@@ -1,22 +1,44 @@
 package edu.westga.cs3212.group5.nutritiontracker.view;
 
+import com.jfoenix.controls.JFXHamburger;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
 
 public class CreateFoodItemTypeSelectionPageController {
+
     @FXML
     private ResourceBundle resources;
+
     @FXML
     private URL location;
+
+    @FXML
+    private JFXHamburger hamburgerMenu;
+
+    @FXML
+    private Button homeButton;
+
+    @FXML
+    private Button logoutButton;
+
+    @FXML
+    private Pane menuPane;
+
     @FXML
     private Button selectBaseFoodButton;
+
     @FXML
     private Button selectCompositFoodButton;
 
     @FXML
     void initialize() {
+        assert hamburgerMenu != null : "fx:id=\"hamburgerMenu\" was not injected: check your FXML file 'CreateFoodItemTypeSelectionPage.fxml'.";
+        assert homeButton != null : "fx:id=\"homeButton\" was not injected: check your FXML file 'CreateFoodItemTypeSelectionPage.fxml'.";
+        assert logoutButton != null : "fx:id=\"logoutButton\" was not injected: check your FXML file 'CreateFoodItemTypeSelectionPage.fxml'.";
+        assert menuPane != null : "fx:id=\"menuPane\" was not injected: check your FXML file 'CreateFoodItemTypeSelectionPage.fxml'.";
         assert selectBaseFoodButton != null : "fx:id=\"selectBaseFoodButton\" was not injected: check your FXML file 'CreateFoodItemTypeSelectionPage.fxml'.";
         assert selectCompositFoodButton != null : "fx:id=\"selectCompositFoodButton\" was not injected: check your FXML file 'CreateFoodItemTypeSelectionPage.fxml'.";
 
