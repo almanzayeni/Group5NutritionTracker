e64c5b37b3691904957231e4f0deddf0859b9359
jsmith5721 <161377944+jsmith5721@users.noreply.github.com>
2026-03-02 12:13:43 -0500

    Tested CreateCompostieFoodPageVM getIngredient
    Added testing for the getIngredient method in
    CreateCompositeFoodPageViewModel with all tests passing and 100%
    coverage.

--- code/NutritionTracker/src/test/java/edu/westga/cs3212/group5/nutritiontracker/viewmodel/createcompositfoodpageviewmodel/TestGetIngredient.java
+++ code/NutritionTracker/src/test/java/edu/westga/cs3212/group5/nutritiontracker/viewmodel/createcompositfoodpageviewmodel/TestGetIngredient.java
@@ -1,5 +1,84 @@
 package edu.westga.cs3212.group5.nutritiontracker.viewmodel.createcompositfoodpageviewmodel;
 
-public class TestGetIngredient {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs3212.group5.nutritiontracker.model.BaseFood;
+import edu.westga.cs3212.group5.nutritiontracker.model.QuantityCategory;
+import edu.westga.cs3212.group5.nutritiontracker.viewmodel.CreateCompositeFoodPageViewModel;
+
+public class TestGetIngredient {
+	@Test
+	public void testNullIngredient() {
+		CreateCompositeFoodPageViewModel viewModel = new CreateCompositeFoodPageViewModel();
+		assertThrows(IllegalArgumentException.class, () -> {
+			viewModel.getIngredient(null);
+		});
+	}
+	
+	@Test
+	public void testNonExistentIngredientWithEmptyIngredientsList() {
+		CreateCompositeFoodPageViewModel viewModel = new CreateCompositeFoodPageViewModel();
+		BaseFood ingredient = new BaseFood("Test Ingredient", QuantityCategory.WEIGHT, 1, 100, 10, 1, 1, 1, 1);
+		assertThrows(IllegalArgumentException.class, () -> {
+			viewModel.getIngredient(ingredient);
+		});
+	}
+	
+	@Test
+	public void testNonExistentIngredientWithNonEmptyIngredientsList() {
+		CreateCompositeFoodPageViewModel viewModel = new CreateCompositeFoodPageViewModel();
+		BaseFood ingredient1 = new BaseFood("Ingredient 1", QuantityCategory.WEIGHT, 1, 100, 10, 1, 1, 1, 1);
+		BaseFood ingredient2 = new BaseFood("Ingredient 2", QuantityCategory.QUANTITY, 1, 200, 20, 2, 2, 2, 2);
+		viewModel.addIngredient(ingredient1);
+		assertThrows(IllegalArgumentException.class, () -> {
+			viewModel.getIngredient(ingredient2);
+		});
+	}
+	
+	@Test
+	public void testExistingIngredientOnlyIngredient() {
+		CreateCompositeFoodPageViewModel viewModel = new CreateCompositeFoodPageViewModel();
+		BaseFood ingredient = new BaseFood("Test Ingredient", QuantityCategory.WEIGHT, 1, 100, 10, 1, 1, 1, 1);
+		viewModel.addIngredient(ingredient);
+		assertEquals(ingredient, viewModel.getIngredient(ingredient));
+	}
+	
+	@Test
+	public void testExistingIngredientFirstPositionMultipleIngredients() {
+		CreateCompositeFoodPageViewModel viewModel = new CreateCompositeFoodPageViewModel();
+		BaseFood ingredient1 = new BaseFood("Ingredient 1", QuantityCategory.WEIGHT, 1, 100, 10, 1, 1, 1, 1);
+		BaseFood ingredient2 = new BaseFood("Ingredient 2", QuantityCategory.QUANTITY, 1, 200, 20, 2, 2, 2, 2);
+		BaseFood ingredient3 = new BaseFood("Ingredient 3", QuantityCategory.SERVING, 1, 300, 30, 3, 3, 3, 3);
+		viewModel.addIngredient(ingredient1);
+		viewModel.addIngredient(ingredient2);
+		viewModel.addIngredient(ingredient3);
+		assertEquals(ingredient1, viewModel.getIngredient(ingredient1));
+	}
+	
+	@Test
+	public void testExistingIngredientMiddlePositionMultipleIngredients() {
+		CreateCompositeFoodPageViewModel viewModel = new CreateCompositeFoodPageViewModel();
+		BaseFood ingredient1 = new BaseFood("Ingredient 1", QuantityCategory.WEIGHT, 1, 100, 10, 1, 1, 1, 1);
+		BaseFood ingredient2 = new BaseFood("Ingredient 2", QuantityCategory.QUANTITY, 1, 200, 20, 2, 2, 2, 2);
+		BaseFood ingredient3 = new BaseFood("Ingredient 3", QuantityCategory.SERVING, 1, 300, 30, 3, 3, 3, 3);
+		viewModel.addIngredient(ingredient1);
+		viewModel.addIngredient(ingredient2);
+		viewModel.addIngredient(ingredient3);
+		assertEquals(ingredient2, viewModel.getIngredient(ingredient2));
+	}
+	
+	@Test
+	public void testExistingIngredientLastPositionMultipleIngredients() {
+		CreateCompositeFoodPageViewModel viewModel = new CreateCompositeFoodPageViewModel();
+		BaseFood ingredient1 = new BaseFood("Ingredient 1", QuantityCategory.WEIGHT, 1, 100, 10, 1, 1, 1, 1);
+		BaseFood ingredient2 = new BaseFood("Ingredient 2", QuantityCategory.QUANTITY, 1, 200, 20, 2, 2, 2, 2);
+		BaseFood ingredient3 = new BaseFood("Ingredient 3", QuantityCategory.SERVING, 1, 300, 30, 3, 3, 3, 3);
+		viewModel.addIngredient(ingredient1);
+		viewModel.addIngredient(ingredient2);
+		viewModel.addIngredient(ingredient3);
+		assertEquals(ingredient3, viewModel.getIngredient(ingredient3));
+	}
 }
