62c0b688212716e2d9165c00099af4b9df048107
Yeni Almanza <ya00013@my.westga.edu>
2026-03-31 19:35:15 -0400

    Add create food client
    Add create food handler and server constants.

--- code/NutritionTracker/src/main/java/edu/westga/cs3212/group5/nutritiontracker/server/ServerConstants.java
+++ code/NutritionTracker/src/main/java/edu/westga/cs3212/group5/nutritiontracker/server/ServerConstants.java
@@ -20,10 +20,13 @@ public class ServerConstants {
 	public static final String KEY_USER = "user";
 	public static final String KEY_USERNAME = "username";
 	public static final String KEY_PASSWORD = "password";
+	public static final String KEY_FOOD_ITEM = "food_item";
 	
 	public static final String SUCCESS_STATUS = "1";
 	public static final String BAD_MESSAGE_STATUS = "-1";
 	public static final String UNSUPPORTED_OPERATION_STATUS = "-1";
 	
 	public static final String AUTHENTICATE_LOGIN_REQUEST_TYPE = "AUTH";
+	public static final String ADD_FOOD_REQUEST_TYPE = "ADD_FOOD";
+	
 }
