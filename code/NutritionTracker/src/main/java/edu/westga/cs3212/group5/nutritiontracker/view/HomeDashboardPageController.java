2a18bb28b5151906150771e13572c436835817f7
Yeni Almanza <ya00013@my.westga.edu>
2026-04-15 15:36:09 -0400

    disable add food
    Set add food back to how it was

--- code/NutritionTracker/src/main/java/edu/westga/cs3212/group5/nutritiontracker/view/HomeDashboardPageController.java
+++ code/NutritionTracker/src/main/java/edu/westga/cs3212/group5/nutritiontracker/view/HomeDashboardPageController.java
@@ -201,7 +201,7 @@ public class HomeDashboardPageController implements ViewModelAware {
     private void setupMenuButtons() {
         this.homeButton.setOnAction(e -> this.switchTo("HomeDashboardPage.fxml"));
         this.createFoodButton.setOnAction(e -> this.switchTo("CreateFoodItemTypeSelectionPage.fxml"));
-        this.addFoodButton.setOnAction(e -> this.switchTo("SearchPage.fxml"));
+        //this.addFoodButton.setOnAction(e -> this.switchTo("SearchPage.fxml"));
         this.logoutButton.setOnAction(e -> this.handleLogout());
         
     }
