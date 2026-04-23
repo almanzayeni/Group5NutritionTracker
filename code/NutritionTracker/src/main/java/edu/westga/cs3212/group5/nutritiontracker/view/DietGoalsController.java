1a16e9775a2400ab864e716ff5bc6128f270eb8f
Yeni Almanza <ya00013@my.westga.edu>
2026-04-23 16:12:46 -0400

    Add menus to edit diet plan page
    Add the hamburger menu and the account menu to the
    diet goals page.

--- code/NutritionTracker/src/main/java/edu/westga/cs3212/group5/nutritiontracker/view/DietGoalsController.java
+++ code/NutritionTracker/src/main/java/edu/westga/cs3212/group5/nutritiontracker/view/DietGoalsController.java
@@ -1,7 +1,11 @@
 package edu.westga.cs3212.group5.nutritiontracker.view;
 
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
+
+import com.jfoenix.controls.JFXHamburger;
+import com.jfoenix.transitions.hamburger.HamburgerSlideCloseTransition;
 
 import edu.westga.cs3212.group5.nutritiontracker.model.DietGoals;
 import edu.westga.cs3212.group5.nutritiontracker.model.PrimaryGoal;
@@ -13,10 +17,14 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.util.converter.NumberStringConverter;
 
@@ -63,6 +71,20 @@ public class DietGoalsController implements ViewModelAware {
 
     @FXML
     private Label warningLabel;
+    @FXML
+    private JFXHamburger hamburgerMenu;
+
+    @FXML
+    private Pane menuPane;
+
+    @FXML
+    private Button homeButton;
+
+    @FXML
+    private Button createFoodButton;
+
+    @FXML
+    private MenuButton accountMenu;
 
     private DietGoalsViewModel viewModel;
     private HomeDashboardViewModel homeDashboardViewModel;
@@ -89,6 +111,16 @@ public class DietGoalsController implements ViewModelAware {
                 : "fx:id=\"sugarTextField\" was not injected: check your FXML file 'PreferencesPage.fxml'.";
         assert this.warningLabel != null
                 : "fx:id=\"warningLabel\" was not injected: check your FXML file 'PreferencesPage.fxml'.";
+        assert this.hamburgerMenu != null
+                : "fx:id=\"hamburgerMenu\" was not injected: check your FXML file 'PreferencesPage.fxml'.";
+        assert this.menuPane != null
+                : "fx:id=\"menuPane\" was not injected: check your FXML file 'PreferencesPage.fxml'.";
+        assert this.homeButton != null
+                : "fx:id=\"homeButton\" was not injected: check your FXML file 'PreferencesPage.fxml'.";
+        assert this.createFoodButton != null
+                : "fx:id=\"createFoodButton\" was not injected: check your FXML file 'PreferencesPage.fxml'.";
+        assert this.accountMenu != null
+                : "fx:id=\"accountMenu\" was not injected: check your FXML file 'PreferencesPage.fxml'.";
 
         this.viewModel = new DietGoalsViewModel();
 
@@ -104,6 +136,100 @@ public class DietGoalsController implements ViewModelAware {
         this.carbsTextField.textProperty().bindBidirectional(this.viewModel.carbsProperty(), converter);
 
         this.warningLabel.textProperty().bind(this.viewModel.warningProperty());
+        this.setUpListeners();
+    }
+    
+    private void setUpListeners() {
+        this.handleHamburgerMenuClick();
+        this.setUpListenerForHomeButton();
+        this.setUpListenerForCreateFoodButton();
+    }
+    
+    private void handleHamburgerMenuClick() {
+        HamburgerSlideCloseTransition transition = new HamburgerSlideCloseTransition(this.hamburgerMenu);
+        transition.setRate(-1);
+        this.menuPane.setVisible(false);
+
+        this.hamburgerMenu.setOnMouseClicked(event -> {
+            transition.setRate(transition.getRate() * -1);
+            transition.play();
+
+            boolean show = !this.menuPane.isVisible();
+            this.menuPane.setVisible(show);
+
+            if (show) {
+                this.menuPane.toFront();
+                this.hamburgerMenu.toFront();
+            }
+        });
+    }
+    
+    private void setUpListenerForHomeButton() {
+        this.homeButton.setOnAction(event -> {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("HomeDashboardPage.fxml"));
+                Parent parent = loader.load();
+
+                Object controller = loader.getController();
+                if (controller instanceof ViewModelAware && this.homeDashboardViewModel != null) {
+                    ((ViewModelAware) controller).setViewModel(this.homeDashboardViewModel);
+                }
+
+                Stage stage = (Stage) this.hamburgerMenu.getScene().getWindow();
+                stage.setScene(new Scene(parent));
+                stage.setTitle("Home");
+                stage.show();
+
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                this.viewModel.warningProperty().set("Could not return to Home page.");
+            }
+        });
+    }
+    
+    private void setUpListenerForCreateFoodButton() {
+        this.createFoodButton.setOnAction(event -> {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("CreateFoodItemTypeSelectionPage.fxml"));
+                Parent parent = loader.load();
+
+                Object controller = loader.getController();
+                if (controller instanceof ViewModelAware && this.homeDashboardViewModel != null) {
+                    ((ViewModelAware) controller).setViewModel(this.homeDashboardViewModel);
+                }
+
+                Stage stage = (Stage) this.hamburgerMenu.getScene().getWindow();
+                stage.setScene(new Scene(parent));
+                stage.setTitle("Create Food");
+                stage.show();
+
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                this.viewModel.warningProperty().set("Could not open Create Food page.");
+            }
+        });
+    }
+    
+    @FXML
+    void handleLogout(ActionEvent event) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Are you sure you want to logout?");
+        alert.getButtonTypes().setAll(ButtonType.YES, ButtonType.NO);
+
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.isPresent() && result.get() == ButtonType.YES) {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("LoginPage.fxml"));
+                Parent parent = loader.load();
+
+                Stage stage = (Stage) this.accountMenu.getScene().getWindow();
+                stage.setScene(new Scene(parent));
+                stage.setTitle("Login");
+                stage.show();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                this.viewModel.warningProperty().set("Could not return to Login page.");
+            }
+        }
     }
 
     @FXML
