03c9d8eb58f457a173bf245b5271da8183308109
Yeni Almanza <ya00013@my.westga.edu>
2026-04-22 23:48:39 -0400

    Edited preferences/diet goal page
    Cahnged title to Diet Goals, removed create account button and
    back label. Instead added a save button that return to the
    dashbaord page. No diet goals actually save for the current user.

--- code/NutritionTracker/src/main/java/edu/westga/cs3212/group5/nutritiontracker/view/DietGoalsController.java
+++ code/NutritionTracker/src/main/java/edu/westga/cs3212/group5/nutritiontracker/view/DietGoalsController.java
@@ -3,120 +3,148 @@ package edu.westga.cs3212.group5.nutritiontracker.view;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import edu.westga.cs3212.group5.nutritiontracker.model.PrimaryGoal;
 import edu.westga.cs3212.group5.nutritiontracker.model.DietGoals;
+import edu.westga.cs3212.group5.nutritiontracker.model.PrimaryGoal;
 import edu.westga.cs3212.group5.nutritiontracker.viewmodel.DietGoalsViewModel;
+import edu.westga.cs3212.group5.nutritiontracker.viewmodel.HomeDashboardViewModel;
+import edu.westga.cs3212.group5.nutritiontracker.viewmodel.ViewModelAware;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import javafx.util.converter.NumberStringConverter;
 
 /**
  * Controller for the Preferences page.
  *
  * @author Yeni Almanza
- * @version spring 2026
+ * @version Spring 2026
  */
-public class DietGoalsController {
+public class DietGoalsController implements ViewModelAware {
 
-	@FXML
-	private ResourceBundle resources;
+    @FXML
+    private ResourceBundle resources;
 
-	@FXML
-	private URL location;
+    @FXML
+    private URL location;
 
-	@FXML
-	private Label backButtonLabel;
+    @FXML
+    private TextField calorieTextField;
 
-	@FXML
-	private TextField calorieTextField;
+    @FXML
+    private TextField carbsTextField;
 
-	@FXML
-	private TextField carbsTextField;
+    @FXML
+    private Button saveButton;
 
-	@FXML
-	private Button createAccountButton;
+    @FXML
+    private TextField fatTextField;
 
-	@FXML
-	private TextField fatTextField;
+    @FXML
+    private TextField otherGoalsTextField;
 
-	@FXML
-	private TextField otherGoalsTextField;
+    @FXML
+    private ComboBox<PrimaryGoal> primaryGoalComboBox;
 
-	@FXML
-	private ComboBox<PrimaryGoal> primaryGoalComboBox;
+    @FXML
+    private TextField proteinTextField;
 
-	@FXML
-	private TextField proteinTextField;
+    @FXML
+    private TextField sodiumTextField;
 
-	@FXML
-	private TextField sodiumTextField;
+    @FXML
+    private TextField sugarTextField;
 
-	@FXML
-	private TextField sugarTextField;
+    @FXML
+    private Label warningLabel;
 
-	@FXML
-	private Label warningLabel;
+    private DietGoalsViewModel viewModel;
+    private HomeDashboardViewModel homeDashboardViewModel;
 
-	private DietGoalsViewModel viewModel;
+    @FXML
+    void initialize() {
+        assert this.calorieTextField != null
+                : "fx:id=\"calorieTextField\" was not injected: check your FXML file 'PreferencesPage.fxml'.";
+        assert this.carbsTextField != null
+                : "fx:id=\"carbsTextField\" was not injected: check your FXML file 'PreferencesPage.fxml'.";
+        assert this.saveButton != null
+                : "fx:id=\"saveButton\" was not injected: check your FXML file 'PreferencesPage.fxml'.";
+        assert this.fatTextField != null
+                : "fx:id=\"fatTextField\" was not injected: check your FXML file 'PreferencesPage.fxml'.";
+        assert this.otherGoalsTextField != null
+                : "fx:id=\"otherGoalsTextField\" was not injected: check your FXML file 'PreferencesPage.fxml'.";
+        assert this.primaryGoalComboBox != null
+                : "fx:id=\"primaryGoalComboBox\" was not injected: check your FXML file 'PreferencesPage.fxml'.";
+        assert this.proteinTextField != null
+                : "fx:id=\"proteinTextField\" was not injected: check your FXML file 'PreferencesPage.fxml'.";
+        assert this.sodiumTextField != null
+                : "fx:id=\"sodiumTextField\" was not injected: check your FXML file 'PreferencesPage.fxml'.";
+        assert this.sugarTextField != null
+                : "fx:id=\"sugarTextField\" was not injected: check your FXML file 'PreferencesPage.fxml'.";
+        assert this.warningLabel != null
+                : "fx:id=\"warningLabel\" was not injected: check your FXML file 'PreferencesPage.fxml'.";
 
-	@FXML
-	void handleCreateAccount(ActionEvent event) {
-		PrimaryGoal selectedGoal = this.primaryGoalComboBox.getValue();
-		if (selectedGoal == null) {
-			this.warningLabel.setText("Please select a primary goal.");
-			return;
-		}
+        this.viewModel = new DietGoalsViewModel();
 
-		String otherGoals = this.otherGoalsTextField.getText();
-		DietGoals prefs = this.viewModel.createDietGoals(selectedGoal, otherGoals);
+        this.primaryGoalComboBox.getItems().setAll(PrimaryGoal.values());
+        this.primaryGoalComboBox.setValue(PrimaryGoal.CALORIE);
 
-		if (prefs != null) {
-			// TODO: pass prefs to account-creation logic / navigate to next screen
-		}
-	}
+        NumberStringConverter converter = new NumberStringConverter();
+        this.calorieTextField.textProperty().bindBidirectional(this.viewModel.calorieProperty(), converter);
+        this.proteinTextField.textProperty().bindBidirectional(this.viewModel.proteinProperty(), converter);
+        this.fatTextField.textProperty().bindBidirectional(this.viewModel.fatProperty(), converter);
+        this.sugarTextField.textProperty().bindBidirectional(this.viewModel.sugarProperty(), converter);
+        this.sodiumTextField.textProperty().bindBidirectional(this.viewModel.sodiumProperty(), converter);
+        this.carbsTextField.textProperty().bindBidirectional(this.viewModel.carbsProperty(), converter);
 
-	@FXML
-	void initialize() {
-		assert this.backButtonLabel != null
-				: "fx:id=\"backButtonLabel\" was not injected: check your FXML file 'PreferencesPage.fxml'.";
-		assert this.calorieTextField != null
-				: "fx:id=\"calorieTextField\" was not injected: check your FXML file 'PreferencesPage.fxml'.";
-		assert this.carbsTextField != null
-				: "fx:id=\"carbsTextField\" was not injected: check your FXML file 'PreferencesPage.fxml'.";
-		assert this.createAccountButton != null
-				: "fx:id=\"createAccountButton\" was not injected: check your FXML file 'PreferencesPage.fxml'.";
-		assert this.fatTextField != null
-				: "fx:id=\"fatTextField\" was not injected: check your FXML file 'PreferencesPage.fxml'.";
-		assert this.otherGoalsTextField != null
-				: "fx:id=\"otherGoalsTextField\" was not injected: check your FXML file 'PreferencesPage.fxml'.";
-		assert this.primaryGoalComboBox != null
-				: "fx:id=\"primaryGoalComboBox\" was not injected: check your FXML file 'PreferencesPage.fxml'.";
-		assert this.proteinTextField != null
-				: "fx:id=\"proteinTextField\" was not injected: check your FXML file 'PreferencesPage.fxml'.";
-		assert this.sodiumTextField != null
-				: "fx:id=\"sodiumTextField\" was not injected: check your FXML file 'PreferencesPage.fxml'.";
-		assert this.sugarTextField != null
-				: "fx:id=\"sugarTextField\" was not injected: check your FXML file 'PreferencesPage.fxml'.";
-		assert this.warningLabel != null
-				: "fx:id=\"warningLabel\" was not injected: check your FXML file 'PreferencesPage.fxml'.";
+        this.warningLabel.textProperty().bind(this.viewModel.warningProperty());
+    }
 
-		this.viewModel = new DietGoalsViewModel();
+    @FXML
+    void handleSave(ActionEvent event) {
+        PrimaryGoal selectedGoal = this.primaryGoalComboBox.getValue();
+        if (selectedGoal == null) {
+            this.viewModel.warningProperty().set("Please select a primary goal.");
+            return;
+        }
 
-		this.primaryGoalComboBox.getItems().setAll(PrimaryGoal.values());
-		this.primaryGoalComboBox.setValue(PrimaryGoal.CALORIE);
+        String otherGoals = this.otherGoalsTextField.getText();
+        DietGoals prefs = this.viewModel.createDietGoals(selectedGoal, otherGoals);
 
-		NumberStringConverter converter = new NumberStringConverter();
-		this.calorieTextField.textProperty().bindBidirectional(this.viewModel.calorieProperty(), converter);
-		this.proteinTextField.textProperty().bindBidirectional(this.viewModel.proteinProperty(), converter);
-		this.fatTextField.textProperty().bindBidirectional(this.viewModel.fatProperty(), converter);
-		this.sugarTextField.textProperty().bindBidirectional(this.viewModel.sugarProperty(), converter);
-		this.sodiumTextField.textProperty().bindBidirectional(this.viewModel.sodiumProperty(), converter);
-		this.carbsTextField.textProperty().bindBidirectional(this.viewModel.carbsProperty(), converter);
+        if (prefs == null) {
+            return;
+        }
 
-		this.warningLabel.textProperty().bind(this.viewModel.warningProperty());
-	}
+        try {
+            // TODO: save prefs to the current user/account here
+
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("HomeDashboardPage.fxml"));
+            Parent parent = loader.load();
+
+            Object controller = loader.getController();
+            if (controller instanceof ViewModelAware && this.homeDashboardViewModel != null) {
+                ((ViewModelAware) controller).setViewModel(this.homeDashboardViewModel);
+            }
+
+            Stage stage = (Stage) this.saveButton.getScene().getWindow();
+            stage.setScene(new Scene(parent));
+            stage.setTitle("Home");
+            stage.show();
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            this.viewModel.warningProperty().set("Could not return to Home page.");
+        }
+    }
+
+    @Override
+    public void setViewModel(HomeDashboardViewModel viewModel) {
+        this.homeDashboardViewModel = viewModel;
+    }
 }
